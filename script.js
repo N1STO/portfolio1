1a5c22ed9963a2e34319554f3805bf82407d4b46
nurillo <108608842+N1STO@users.noreply.github.com>
2022-11-22 16:04:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+/* --------------------------- ASIDE -----------------------------------*/
+
+const nav = document.querySelector(".nav"),
+      navlist = nav.querySelector("li"),
+      totalNavList = navlist.length;
+      for(let i=0; i<totalNavList; i++)
+      {
+        const a = navlist[i].querySelector("a");
+        a.addEventListener("click", function()
+        {
+            for(let j=0; j<totalNavList; j++)
+            {
+                navlist[j].querySelector("a").classList.remove("active");
+            }
+            this.classList.add("active")
+        })
+      }
